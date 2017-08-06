ae5fce94089138e57780b9653527aa1e7e491bf0
cavidann <cavi98@icloud.com>
2017-08-05 23:23:59 -1100

    user added

---
+++ asc_general/Controllers/Controllers/MemorizedController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asc_general.Controllers.Controllers
+{
+    public class MemorizedController : Controller
+    {
+        // GET: Memorized
+        public ActionResult Index()
+        {
+            if (Session["user"] != null)
+            {
+                return View();
+
+            }
+            else
+            {
+                return RedirectToAction("login", "user");
+            }
+        }
+    }
+}
--- asc_general/Controllers/userController.cs
+++ asc_general/Controllers/userController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using asc_general.Models;
 using System.Dynamic;
 using System.Net;
+using System.Web.Helpers;
 
 namespace asc_general.Controllers
 {
@@ -18,14 +19,6 @@ namespace asc_general.Controllers
         
             if (Session["user"] != null)
             {
-                //if (id == null)
-                //{
-                //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-                //}
-                //dynamic mymodel = new ExpandoObject();
-                //user  user_id = db.users.Find(id);
-                //mymodel.userID = user_id;
-                //mymodel.user  = db.users.ToList();
                 return View();
                 
             }
@@ -72,6 +65,104 @@ namespace asc_general.Controllers
         {
             Session.Clear();
             return RedirectToAction("index", "user");
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult Register(user user)
+        {
+            if (user.username != null && user.password == user.password1 && user.email != null)
+            {
+                user usr = db.users.FirstOrDefault(u => u.email == user.email);
+                if (usr == null)
+                {
+                    //user.password = Crypto.Hash(user.password, "MD5");
+                    db.users.Add(user);
+                    db.SaveChanges();
+
+
+                    Session["user"] = true;
+                    var response = true;
+                    return Json(response, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    var response = false;
+                    return Json(response, JsonRequestBehavior.AllowGet);
+
+                }
+
+            }
+            else
+            {
+                var response = false;
+                return Json(response, JsonRequestBehavior.AllowGet);
+            }
+            //  return View();
+        }
+
+        [HttpPost]
+        public JsonResult checkEmail(string email)
+        {
+            if (email.Length > 0)
+            {
+                user user = db.users.FirstOrDefault(usr => usr.email == email);
+                if (user != null)
+                {
+                    var response = new
+                    {
+                        valid = false,
+                        message = "This email already exists!"
+                    };
+                    return Json(response, JsonRequestBehavior.AllowGet);
+
+                }
+                else
+                {
+                    var response = new
+                    {
+                        valid = true,
+                    };
+                    return Json(response, JsonRequestBehavior.AllowGet);
+
+                }
+
+            }
+            else
+            {
+                var response = new
+                {
+                    valid = false,
+                    message = "Email is required"
+                };
+                return Json(response, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [HttpPost]
+        public JsonResult checkPassword(string password, string password1)
+        {
+            if (password==password1)
+            {
+                
+                    var response = new
+                    {
+                        valid = true,
+                        message = "matched"
+                    };
+                    return Json(response, JsonRequestBehavior.AllowGet);               
+
+            }
+            else
+            {
+                var response = new
+                {
+                    valid = false,
+                    message = "not macthed"
+                };
+                return Json(response, JsonRequestBehavior.AllowGet);
+            }
         }
 
     }
